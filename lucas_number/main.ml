e02fd32b0381e7b1493794a731e5720be9bddf85
Akitsu Tsujimoto <43626780+RockwallNest@users.noreply.github.com>
2022-01-14 17:20:36 +0900

    Update README.md
    Add Comments why using Unit-type argument in recursive function or so on.

--- lucas_number/main.ml
+++ lucas_number/main.ml
@@ -1,20 +1,29 @@
 (* lucas_number/main.ml *)
 (* 
-  ~~ Dynamic Programming ~~
+  ~~ Dynamic Programming: dp ~~
   Bottom-up method: Recurrence formula loop  
 *)
 
 let () =
   let main () =
     let n = read_int () in
+    (* Make n pieces of dp-Array with initial value 0 *)
     let dp = Array.init (n+2) (fun i -> 0) in
+    (* Substitute two initial integer numbers for {each value of dp-Array: 0}. *)
     dp.(0) <- 2;
     dp.(1) <- 1;
     let result =
+    (* Create Recursive function: `loop j () dp` *)
       let rec loop j () dp =
+      (* Becase of `j` is up to `n` in Recursive function `loop` , if-clause is set in `j = (n+1)` *)
         if j = (n+1) then dp.(n)
         else 
+        (* 
+          It is necessary for the Recursive function `loop` to assign its argument to {the `Unit-type` value: `()`}
+          if you substitute new value for dp-Array's value recursively. 
+        *) 
           loop (j+1) (dp.(j) <- (dp.(j-1) + dp.(j-2))) dp
+          (* Calculate {third dp-Array's value: dp.(2) = 3} wtih {initial dp-Array's value: 2 and 1} *)
     in loop 2 (dp.(2) <- 3) dp
       in Printf.printf "%d\n" result
     in main ();;
