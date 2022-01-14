9395ebe0236d12a013037cb7a8f8f9cee072c471
Akitsu Tsujimoto <43626780+RockwallNest@users.noreply.github.com>
2022-01-14 15:20:02 +0900

    Create My OCaml code for AtCorder
    This OCaml exe file is to calculate Lucas Number.
    With omake it makes OCaml main.exe from OCaml source code (main.ml).

---
+++ lucas_number/main.ml
@@ -0,0 +1,15 @@
+let () =
+  let main () =
+    let n = read_int () in
+    let dp = Array.init (n+2) (fun i -> 0) in
+    dp.(0) <- 2;
+    dp.(1) <- 1;
+    let result =
+      let rec loop j () dp =
+        if j = (n+1) then dp.(n)
+        else 
+          loop (j+1) (dp.(j) <- (dp.(j-1) + dp.(j-2))) dp
+    in loop 2 (dp.(2) <- 3) dp
+      in Printf.printf "%d\n" result
+    in main ();;
+    
